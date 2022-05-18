fc661d07ef42ea26e759970f65be87c65a3b1411
Valdeci97 <moura97.valdeci@gmail.com>
2022-05-18 18:53:17 -0300

    NEW: Initial database configuration

---
+++ app/backend/src/database/config/database.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: process.env.DB_NAME || 'task_manager',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3002,
+  dialect: 'mysql',
+  dialectOptions: { timezone: 'Z' },
+};
+
+export default config;
