f9e823846679607bbacc3d75c75ee01dade220a6
Valdeci97 <moura97.valdeci@gmail.com>
2022-05-19 11:01:44 -0300

    NEW: Create login test and user's service n controller

---
+++ app/backend/src/database/services/user.ts
@@ -0,0 +1,19 @@
+import User from '../models/user';
+
+class UserService {
+  private _user;
+
+  constructor() { this._user = User; }
+
+  public async login(email: string, _password: string) {
+    const user = await this._user.findOne({ where: { email } });
+    if (!user) {
+      return { status: 401, message: 'Incorrect email or password' };
+    }
+    return {
+      user,
+    };
+  }
+}
+
+export default new UserService();
