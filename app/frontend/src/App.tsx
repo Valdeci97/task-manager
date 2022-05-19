800f0ef0342e6f86057a7edaad271c40e799eaff
Valdeci97 <moura97.valdeci@gmail.com>
2022-05-19 19:20:40 -0300

    NEW: Create frontend folders

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world from typescript</p>
-    </div>
+    <Login />
   );
 }
 
