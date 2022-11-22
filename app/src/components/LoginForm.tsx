78b2bd72632c71aef1368c2f52437ddcc00ee05c
valdeci <moura97.valdeci@gmail.com>
2022-11-22 17:07:19 -0300

    feat(components): create login form

---
+++ app/src/components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Label from './Label';
+import Button from './Button';
+import { SL } from '../styles/login';
+import PasswordLabel from './PasswordLabel';
+
+export default function LoginForm() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <>
+      <SL.Container>
+        <Label
+          id={'email-input'}
+          value={email}
+          handleChange={setEmail}
+          text={'e-mail'}
+        />
+        <PasswordLabel
+          type="password"
+          id={'password-input'}
+          value={password}
+          handleChange={setPassword}
+          text={'senha'}
+        />
+        <Button text="Entrar" />
+      </SL.Container>
+    </>
+  );
+}
