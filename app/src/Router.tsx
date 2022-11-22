b9bcb0adadac6b21fa97f110deaa0bab5c640cc7
valdeci <moura97.valdeci@gmail.com>
2022-11-22 17:09:28 -0300

    build(app): create router

---
+++ app/src/Router.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Navigate to="/login" replace /> } />
+      <Route path="/login" element={ <Login /> } />
+    </Routes>
+  );
+}
