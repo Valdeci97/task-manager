47ba4497b4c1b25cdca9f9891670ccc9af4c633b
valdeci <moura97.valdeci@gmail.com>
2022-12-16 22:34:34 -0300

    build(docker): create docker and docker-compose files

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 5000 },
   plugins: [react()],
   test: {
     globals: true,
